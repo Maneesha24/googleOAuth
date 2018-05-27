93003b918d0e36f5cc702e3b152d7bef3bd77f40
Maneesha <venigallamaneesha24@gmail.com>
2018-05-27 06:20:34 +0530

    final changes

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import React from "react";
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+import Header from "./header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Landing from './landing';
 
-const App = () =>{
-  return(
-    <div>Hey</div>
-  )
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null,actions)(App);
---
+++ client/src/components/header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+export default class Header extends React.Component {
+renderContent(){
+  switch(this.props.auth){
+    case null:
+    return ;
+    case false:
+    return(
+      <li><a href= "/auth/google">Login</a></li>
+    )
+    default:
+    return (<li><a href = "/api/logout">Logout</a></li>)
+  }
+}
+  render() {
+    return (
+      <nav>
+              <div className="nav-wrapper">
+                <Link
+                  to={this.props.auth ? '/surveys' : '/'}
+                  className="left brand-logo"
+                >
+                  googleOauth
+                </Link>
+                <ul className="right">
+                  {this.renderContent()}
+                </ul>
+              </div>
+            </nav>
+    );
+  }
+}
