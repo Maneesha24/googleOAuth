500002854c221fbb2bfa6a07088d9594ec098736
Maneesha <venigallamaneesha24@gmail.com>
2018-06-08 02:18:20 +0530

    adds changes

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,31 +1,34 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-
 
 export default class Header extends React.Component {
-renderContent(){
-  switch(this.props.auth){
-    case null:
-    return ;
-    case false:
-    return(
-      <li><a href= "/auth/google">Login</a></li>
-    )
-    default:
-    return (<li><a href = "/api/logout">Logout</a></li>)
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
   }
-}
   render() {
     return (
       <nav>
-              <div className="nav-wrapper">
-
-
-                <ul className="right">
-                  {this.renderContent()}
-                </ul>
-              </div>
-            </nav>
+        <div className="nav-wrapper">
+          <ul className="left ">
+            <li className="">googleOAuth</li>
+          </ul>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
     );
   }
 }
